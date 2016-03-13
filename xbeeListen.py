8441694ca7ca9db7eb1c6a0048e8ea5f24dd5dd3
Aaron Walker <busukajw@gmail.com>
2016-03-13 14:23:36 +0000

    added some basic logging and tidied an a little

--- xbeeListen.py
+++ xbeeListen.py
@@ -1,4 +1,4 @@
- # ZigBee Router Low Address 40C82BEC
+# ZigBee Router Low Address 40C82BEC
  # PANID
  # Zibbee Co-Ordinator Address 40C822DD
 
@@ -8,13 +8,10 @@ import time
 import struct
 import json
 import requests
+import logging
 from Queue import Queue
 from xbee import ZigBee
-from collections import namedtuple
 
-PORT = '/dev/ttyAMA0'
-BAUD_RATE = 9600
-serial_port = serial.Serial(PORT, BAUD_RATE)
 message = Queue()
 
 def message_recieved(data):
@@ -23,15 +20,14 @@ def message_recieved(data):
 def post_influx(data):
     payload = "pulse,utility=electric value={0}\n".format(data['electric'])
     payload += "pulse,utility=gas value={0}\n".format(data['gas'])
-    print payload
     try:
-        r=requests.post("http://192.168.0.14:8086/write?db=mydb", data=payload)
+        r=requests.post("http://192.168.0.14:8086/write?db=mydb",timeout=(3.05, 6), data=payload)
+        if r.status_code == 204:
+            logging.info('http-status: %s: payload: %s', r.status_code, payload.replace('\n',','))
     except requests.exceptions.RequestException, e:
-        r = e
-    return r
+        logging.error('%s', e)
 
 
-zb = ZigBee(serial_port, callback=message_recieved)
 
 # zb.send('at', command='SH')
 # print zb.wait_read_frame()['parameter'].encode('hex')
@@ -41,16 +37,33 @@ def process_message(data):
     rf_data = struct.unpack('<BBB',data['rf_data'])
     if rf_data[0] == 1:
         data = {'gas' : rf_data[1], 'electric': rf_data[2]}
-    status = post_influx(data)
-    print status.text
+    post_influx(data)
 
-while True:
+def main():
+    PORT = '/dev/ttyAMA0'
+    BAUD_RATE = 9600
+
+    logging.basicConfig(filename='xListen.log',
+        level=logging.DEBUG,
+        format='%(asctime)s %(message)s')
     try:
-        time.sleep(.2)
-        if message.qsize() > 0:
-            msg = message.get_nowait()
-            process_message(msg)
-    except KeyboardInterrupt:
-        break
-zb.halt()
-serial_port.close()
+        serial_port = serial.Serial(PORT, BAUD_RATE)
+    except serial.SerialException, e:
+        logging.error('%s', e)
+
+    zb = ZigBee(serial_port, callback=message_recieved)
+    logging.info('Starting to Listen for Xbees')
+    while True:
+        try:
+            time.sleep(.2)
+            if message.qsize() > 0:
+                logging.debug('message queue: %s ', message.qsize())
+                msg = message.get_nowait()
+                process_message(msg)
+        except KeyboardInterrupt:
+            break
+    zb.halt()
+    serial_port.close()
+
+if __name__ == '__main__':
+    main()
